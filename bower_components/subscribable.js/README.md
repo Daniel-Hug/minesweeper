49dd091857882e88cd04179a1421122852aee3a8
Daniel Hug <daniel@hugwebdesign.com>
2016-01-09 22:21:27 -0800

    clean up demo.js, update subscribable.js

--- bower_components/subscribable.js/README.md
+++ bower_components/subscribable.js/README.md
@@ -27,7 +27,6 @@ Daniel.trigger('puke', 'That was gross!');
 //=> Eeeww! That was gross!
 ```
 
-
 ## Add events to any constructor
 
 In this example the `Person` contructor inherits its event subscribing capability from `Subscribable`:
@@ -56,12 +55,25 @@ Daniel.trigger('smile', 30);
 //=> Oh look! Daniel has 30 teeth.
 ```
 
+## Methods
+
+ - **Subscibe to events:** `.on(eventsStr, handlerFn, callNow)`
+   - if `callNow` is truthy, `handlerFn` is called right away.
+   - `eventsStr` should be a space-seperated list of events, e.g. `'start stop'`. if `eventsStr` is `'any'`, then `handlerFn` will be called when any event is triggered on this instance of `Subscribable`, otherwise it will be called whenever an event in `eventsStr` is triggered.
+ - **Trigger events:** `.trigger(eventStr[, value1[, ...[, valueN]]])`
+   - trigger the `eventStr` event. Any subscribing handlers will be called with the same arguments passed to `.trigger()`.
+ - **Unsubscribe from events:** `.off(eventsStr, handlerFn)`
+   - pass no arguments to cancel all subscriptions
+   - pass `eventsStr` (a space-separated list of events) to unsubscribe all functions from those events
+   - or pass `eventsStr` and a function to unsubscribe a specific function from those events
+
 
 ## Extend
 
 Add your own methods to the `Subscribable` prototype:
 
-```
+```js
+// subscribe, then unsubscribe after first trigger
 Subscribable.prototype.once = function(event, fn) {
 	var t = this;
 	this.on(event, function() {
--- js/demo.js
+++ js/demo.js
@@ -1,9 +1,20 @@
+/*
+	helpers
+*/
+
+// Get element by CSS selector
+function qs(selector, scope) {
+	return (scope || document).querySelector(selector);
+}
+
 // removes all of an element's childNodes
 function removeChilds(el) {
 	var last;
 	while ((last = el.lastChild)) el.removeChild(last);
 }
 
+// renderer will be called for each item in arr, and should return a DOM node.
+// all DOM nodes returned will be appended to parent in batch
 function renderMultiple(arr, renderer, parent) {
 	var docFrag = document.createDocumentFragment();
 	[].map.call(arr, renderer).forEach(function(el) {
@@ -12,14 +23,13 @@ function renderMultiple(arr, renderer, parent) {
 	parent.appendChild(docFrag);
 }
 
-// gather elements
-var field = document.getElementById('field');
-var w = document.getElementById('w');
-var h = document.getElementById('h');
-var numMines = document.getElementById('numMines');
-var ms;
 
-// sets cell.element = li;
+
+
+/*
+	render cell
+*/
+
 function renderCell(cell) {
 	var li = document.createElement('li');
 
@@ -49,15 +59,19 @@ function renderCell(cell) {
 	}, false);
 
 	cell.element = li;
-
 	return li;
 }
 
-// generate minesweeper
-function start() {
 
+
+
+/*
+	generate minesweeper
+*/
+
+function start() {
 	// setup game
-	ms = new Minesweeper({
+	var ms = new Minesweeper({
 		width: w.value || 9,
 		height: h.value || 9,
 		cellRenderer: Cell.prototype.render
@@ -65,7 +79,7 @@ function start() {
 	});
 
 	// setup game field element
-	ms.field = document.getElementById('field');
+	ms.field = qs('#field');
 	ms.field.classList.remove('game-won', 'exploded');
 	ms.field.style.width = 24 * ms.width + 'px';
 	removeChilds(field);
@@ -73,22 +87,22 @@ function start() {
 	// render cells and append to game field
 	renderMultiple(ms.cellObjs, renderCell, ms.field);
 
+	// show number of mines
+	qs('#numMines').textContent = ms.numMines;
+
+	// keep flag count element updated
+	(function() {
+		var numFlagged = qs('#numFlagged');
+
+		ms.on('cellFlagToggle', function updateFlagCount() {
+			numFlagged.textContent = ms.numFlagged;
+		}, true);
+	})();
 
 	// toggle 'flagged' class on cell
 	ms.on('cellFlagToggle', function(event, cell) {
 		cell.element.classList.toggle('flagged');
 	});
-
-	// keep flag count element updated
-	(function() {
-		var numFlagged = document.getElementById('numFlagged');
-
-		function updateFlagCount() {
-			numFlagged.textContent = ms.numFlagged;
-		}
-		updateFlagCount();
-		ms.on('cellFlagToggle', updateFlagCount);
-	})();
 
 	// add 'revealed' class to cell
 	ms.on('cellReveal', function(event, cell) {
@@ -104,17 +118,16 @@ function start() {
 	ms.on('win', function() {
 		this.field.classList.add('game-won');
 	});
-
-	// show number of mines
-	numMines.textContent = ms.numMines
 }
 start();
 
-// set initial input values
-w.value = ms.width;
-h.value = ms.height;
 
-// regenerate minesweeper when inputs change
-[w,h].forEach(function(input) {
+
+
+/*
+	regenerate minesweeper when width and height inputs change
+*/
+
+[qs('#w'), qs('#h')].forEach(function(input) {
 	input.addEventListener('input', start);
 });
